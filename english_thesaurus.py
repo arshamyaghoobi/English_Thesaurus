ead2e777491bd71ae8de420b208d843b27365cea
arshamyaghoobi <arshamyaghoobi@yahoo.com>
2020-12-18 19:40:06 -0500

    finilized version 1.0.0

--- english_thesaurus.py
+++ english_thesaurus.py
@@ -8,15 +8,24 @@ def translate(word):
   if word in data:
     return data[word]
   elif len(get_close_matches(word,data.keys())) > 0:
-    return "Did you mean %s instead?" %get_close_matches(word,data.keys())[0] 
+    condition = input("Did you mean %s instead? Enter 'Y' or 'y' if yes or 'N' or 'n' if no: " % get_close_matches(word,data.keys())[0])
+    if condition == 'Y' or condition == 'y':
+      return data[get_close_matches(word,data.keys())[0]]
+    elif condition == 'N' or condition == 'n':
+      return "Please double check your input"  
+    else:
+      return "Invalid option! Try again."
+    
   else:
     return "The word doesn't exist. Please check it." 
 
-  
-
-
-
 word = input("enter word: ")
 
-print(translate(word))
+output = translate(word)
 
+if type(output) == list:
+  for item in output:
+    print(item)
+
+else:
+  print(output)
